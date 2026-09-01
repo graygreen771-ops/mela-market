e222c4db2e489d34142933dd3a2a6b488de8ba80
graygreen771-ops <graygreen771@gmail.com>
2026-09-01 17:04:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+// Get modal elements
+const modal = document.getElementById("paymentModal");
+const orderBtn = document.getElementById("orderBtn");
+const closeBtn = document.getElementsByClassName("close-btn")[0];
+
+// Open modal when "Place Order" is clicked
+orderBtn.onclick = function() {
+    modal.style.display = "flex";
+}
+
+// Close modal when the "X" is clicked
+closeBtn.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Close modal if user clicks outside of the modal box
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
